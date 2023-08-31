eabd89c11bef7fd4f5f4322dd7b9cf4b9c18798d
Sonish <kandelsonish4@gmail.com>
2023-08-31 19:30:59 +0100

    Routes is now working
    - there are 2 pages: Home and PageNotFound
    - there is a routes.js file that contains the routes
    
    - to create a new page, create a new file in src/pages
    - to add a route, add a new line in routes.js

---
+++ 7h-front/src/pages/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PageNotFound() {
+  return (
+    <>
+        <h1>How did you get here?</h1>
+        <p>There's nothing here. 505 error</p>
+    </>
+  );
+}
+
+export default PageNotFound;
